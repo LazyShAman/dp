6f2a4ef5eb63255e60627ac340d44761ecfec4d7
LazyShAman <avs.miln@gmail.com>
2023-05-17 20:12:23 +0300

    Lab3 final code with updated task conditions and documentation

--- 3/dp_Lab3/src/Main.java
+++ 3/dp_Lab3/src/Main.java
@@ -1,122 +1,82 @@
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.security.SecureRandom;
+import javax.crypto.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
+/**
+ * Программа, предназначенная для шифрования изображения
+ * с использованием различных режимов шифрования AES.
+ */
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // Загрузка изображения
-        byte[] imageBytes = loadImage("tux.png");
+    public static void main(String[] args) {
+        String inputFileName = "tux.bmp";
+        byte[] imageData;
+        byte[] header;
+        byte[] encryptedData;
 
-        // Генерация ключа
-        SecretKey key = generateKey();
+        try {
+            // Чтение входного файла изображения
+            imageData = Files.readAllBytes(new File(inputFileName).toPath());
+            header = new byte[54]; // Assuming BMP header size is 54 bytes
+            System.arraycopy(imageData, 0, header, 0, 54);
 
-        // Шифрование и сохранение изображения в режиме ECB
-        byte[] encryptedECB = encryptECB(imageBytes, key);
-        saveImage(encryptedECB, "tux_ecb.png");
+            // Разделение данных заголовка и изображения
+            byte[] imageDataWithoutHeader = new byte[imageData.length - 54];
+            System.arraycopy(imageData, 54, imageDataWithoutHeader, 0, imageData.length - 54);
 
-        // Шифрование и сохранение изображения в режиме CBC
-        byte[] encryptedCBC = encryptInMode(imageBytes, key, "CBC");
-        saveImage(encryptedCBC, "tux_cbc.png");
+            // Сохранение изображения без заголовка в новый файл
+            FileOutputStream outputStream = new FileOutputStream("tux_without_header.bmp");
+            outputStream.write(imageDataWithoutHeader);
+            outputStream.close();
 
-        // Шифрование и сохранение изображения в режиме CFB
-        byte[] encryptedCFB = encryptInMode(imageBytes, key, "CFB");
-        saveImage(encryptedCFB, "tux_cfb.png");
+            // Шифрование и сохранение данных изображения
+            // с помощью различных режимов шифрования AES
+            encryptAndSaveData(imageDataWithoutHeader, header, "ECB");
+            encryptAndSaveData(imageDataWithoutHeader, header, "CBC");
+            encryptAndSaveData(imageDataWithoutHeader, header, "CFB");
+            encryptAndSaveData(imageDataWithoutHeader, header, "OFB");
 
-        // Шифрование и сохранение изображения в режиме OFB
-        byte[] encryptedOFB = encryptInMode(imageBytes, key, "OFB");
-        saveImage(encryptedOFB, "tux_ofb.png");
-    }
-
-    /**
-     * Генерирует ключ для использования в алгоритме AES.
-     *
-     * @return сгенерированный секретный ключ
-     * @throws Exception если произошла ошибка при генерации ключа
-     */
-    public static SecretKey generateKey() throws Exception {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-        keyGenerator.init(128); // Используем ключ длиной 128 бит
-        return keyGenerator.generateKey();
-    }
-
-    /**
-     * Загружает изображение из файла и возвращает его в виде массива байтов.
-     *
-     * @param imagePath путь к файлу изображения
-     * @return массив байтов, представляющих изображение
-     * @throws Exception если произошла ошибка при загрузке изображения
-     */
-    public static byte[] loadImage(String imagePath) throws Exception {
-        FileInputStream fis = new FileInputStream(imagePath);
-        byte[] imageBytes = new byte[fis.available()];
-        fis.read(imageBytes);
-        fis.close();
-        return imageBytes;
-    }
-
-    /**
-     * Сохраняет массив байтов в виде изображения в указанный файл.
-     *
-     * @param imageBytes массив байтов, представляющих изображение
-     * @param imagePath  путь к файлу для сохранения изображения
-     * @throws Exception если произошла ошибка при сохранении изображения
-     */
-    public static void saveImage(byte[] imageBytes, String imagePath) throws Exception {
-        FileOutputStream fos = new FileOutputStream(imagePath);
-        fos.write(imageBytes);
-        fos.close();
-    }
-
-    /**
-     * Шифрует изображение в режиме ECB (Electronic Codebook) с использованием указанного ключа.
-     * В режиме ECB не используется вектор инициализации (IV).
-     *
-     * @param imageBytes массив байтов, представляющих изображение
-     * @param key        секретный ключ для шифрования
-     * @return зашифрованный массив байтов
-     * @throws Exception если произошла ошибка при шифровании
-     */
-    public static byte[] encryptECB(byte[] imageBytes, SecretKey key) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-        return cipher.doFinal(imageBytes);
-    }
-
-    /**
-     * Шифрует изображение в выбранном режиме шифрования (CBC/CFB/OFB) с использованием указанного ключа.
-     * В режимах CBC, CFB и OFB используется вектор инициализации (IV).
-     *
-     * @param imageBytes массив байтов, представляющих изображение
-     * @param key        секретный ключ для шифрования
-     * @param mode       режим шифрования (CBC/CFB/OFB)
-     * @return зашифрованный массив байтов
-     * @throws Exception если произошла ошибка при шифровании
-     */
-    public static byte[] encryptInMode(byte[] imageBytes, SecretKey key, String mode) throws Exception {
-        if (mode.equalsIgnoreCase("ECB")) {
-            return encryptECB(imageBytes, key);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        Cipher cipher = Cipher.getInstance("AES/" + mode + "/PKCS5Padding");
-        byte[] iv = generateIV(cipher.getBlockSize());
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
-        cipher.init(Cipher.ENCRYPT_MODE, key, ivParameterSpec);
-        return cipher.doFinal(imageBytes);
     }
 
     /**
-     * Генерирует случайный вектор инициализации (IV) заданного размера.
-     *
-     * @param blockSize размер блока шифрования
-     * @return сгенерированный вектор инициализации
+     * Метод, который шифрует и сохраняет данные изображения с использованием указанного режима шифрования.
+     * @param imageData данные изображения без заголовка
+     * @param header    заголовок изображения
+     * @param encryptionMode режим шифрования (ECB, CBC, CFB или OFB)
      */
-    public static byte[] generateIV(int blockSize) {
-        byte[] iv = new byte[blockSize];
-        new SecureRandom().nextBytes(iv);
-        return iv;
+    public static void encryptAndSaveData(byte[] imageData, byte[] header, String encryptionMode) {
+        try {
+            // Генерация AES ключа
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(128);
+            SecretKey secretKey = keyGenerator.generateKey();
+
+            // Инициализация шифра с ключом AES и режимом шифрования
+            Cipher cipher = Cipher.getInstance("AES/" + encryptionMode + "/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+            // Шифрование данных изображения
+            byte[] encryptedData = cipher.doFinal(imageData);
+
+            // Объединение заголовка и зашифрованных данных изображения
+            byte[] encryptedImage = new byte[header.length + encryptedData.length];
+            System.arraycopy(header, 0, encryptedImage, 0, header.length);
+            System.arraycopy(encryptedData, 0, encryptedImage, header.length, encryptedData.length);
+
+            // Сохранение зашифрованного изображения в новый файл
+            String outputFileName = "tux" + encryptionMode + "_encrypted.bmp";
+            FileOutputStream outputStream = new FileOutputStream(outputFileName);
+            outputStream.write(encryptedImage);
+            outputStream.close();
+
+            System.out.println("Image encrypted and saved: " + outputFileName);
+
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }

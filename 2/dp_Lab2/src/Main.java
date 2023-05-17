afbe61ac582cceedb4ab9b071363725527959426
LazyShAman <avs.miln@gmail.com>
2023-05-17 12:56:17 +0300

    Lab2 code with documentation

--- 2/dp_Lab2/src/Main.java
+++ 2/dp_Lab2/src/Main.java
@@ -4,10 +4,10 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    static int keyLength = 20;
-    static int imageOffset = 54; // BMP header size is 54 bytes
+    static int keyLength = 20; // Длина ключа шифрования
+    static int imageOffset = 54; // Размер хедера BMP-изображения составляет 54 байта
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         int[] key = generateKey();
         saveKey(key, "key.txt");
 
@@ -26,6 +26,12 @@ public class Main {
         decode(key);
     }
 
+    /**
+     * Метод encode внедряет в BMP-изображение хэшкод файла leasing.txt
+     * с помощью реализации LSB Replacement
+     * @param message сообщение, которое необходимо закодировать
+     * @param key ключ шифрования
+     */
     public static void encode(byte[] message, int[] key) {
         File bmpFile = new File("input.bmp");
         byte[] imageBytes = new byte[(int) bmpFile.length()];
@@ -66,6 +72,11 @@ public class Main {
         System.out.println("Secret message successfully hidden in BMP image!");
     }
 
+    /**
+     * Метод decode извлекает из BMP-изображения хэшкод файла leasing.txt
+     * с помощью реализации LSB Replacement
+     * @param key ключ шифрования
+     */
     public static void decode(int[] key) {
         File bmpFile = new File("image_with_secret.bmp");
         byte[] imageBytes = new byte[(int) bmpFile.length()];
@@ -99,6 +110,12 @@ public class Main {
         System.out.println("Secret message extracted from BMP image: " + hexString);
     }
 
+    /**
+     * Метод contains проверяет наличие передаваемого числа в указанном массиве
+     * @param arr массив чисел
+     * @param val искомое число
+     * @return true/false в зависимости от результатов поиска
+     */
     public static boolean contains(int[] arr, int val) {
         for (int j : arr) {
             if (j == val) {
@@ -108,33 +125,11 @@ public class Main {
         return false;
     }
 
-    public static void saveKey(int[] arr, String filename) {
-        // Сохраняет полученный ключ в файл
-        try (PrintWriter writer = new PrintWriter(new File(filename))) {
-            for (int i = 0; i < keyLength; i++) {
-                writer.println(arr[i]);
-            }
-            System.out.println("Array saved to " + filename);
-        } catch (FileNotFoundException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
-    public static int[] loadKey(String filename) {
-        try (Scanner scanner = new Scanner(new File(filename))) {
-            int[] loadedArr = new int[keyLength];
-            int i = 0;
-            while (scanner.hasNextInt()) {
-                loadedArr[i++] = scanner.nextInt();
-            }
-            System.out.println("Array loaded from " + filename);
-            return loadedArr;
-        } catch (FileNotFoundException e) {
-            System.out.println("Error: " + e.getMessage());
-            return null;
-        }
-    }
-
+    /**
+     * Метод generateKey генерирует рандомные числа на отрезке [1; max]
+     * в количестве keyLength для формирования ключа шифрования
+     * @return ключ шифрования в виде массива чисел
+     */
     public static int[] generateKey() {
         int max = 400;
         int size = keyLength;
@@ -152,6 +147,49 @@ public class Main {
         return key;
     }
 
+    /**
+     * Метод saveKey сохраняет полученный ключ шифрования в файл
+     * @param arr ключ шифрования
+     * @param filename имя создаваемого файла, в который будет сохранен ключ
+     */
+    public static void saveKey(int[] arr, String filename) {
+        try (PrintWriter writer = new PrintWriter(filename)) {
+            for (int i = 0; i < keyLength; i++) {
+                writer.println(arr[i]);
+            }
+            System.out.println("Array saved to " + filename);
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Метод loadKey извлекает из файла записанный ключ шифрования
+     * @param filename имя файла, в котором был сохранен ключ
+     * @return ключ шифрования
+     */
+    public static int[] loadKey(String filename) {
+        try (Scanner scanner = new Scanner(new File(filename))) {
+            int[] loadedArr = new int[keyLength];
+            int i = 0;
+            while (scanner.hasNextInt()) {
+                loadedArr[i++] = scanner.nextInt();
+            }
+            System.out.println("Array loaded from " + filename);
+            return loadedArr;
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * Метод encodeByte внедряет в байтовое представление BMP-изображения байт
+     * шифруемого сообщения, меняя наименьший значащий бит каждого байта пикселей
+     * @param offset позиция шифрования
+     * @param imageBytes массив байтов BMP-изображения
+     * @param messageByte байт шифруемого сообщения
+     */
     public static void encodeByte(int offset, byte[] imageBytes, byte messageByte) {
         for (int i = 0; i < 8; i++) {
             int bit = (messageByte >>> i) & 1;
@@ -160,6 +198,13 @@ public class Main {
         }
     }
 
+    /**
+     * Метод decodeByte извлекает из байтового представления BMP-изображения байт
+     * зашифрованного сообщения
+     * @param offset позиция шифрования
+     * @param imageBytes массив байтов BMP-изображения
+     * @return байт шифруемого сообщения
+     */
     public static byte decodeByte(int offset, byte[] imageBytes) {
         byte messageByte = 0;
         for (int i = 0; i < 8; i++) {
@@ -170,6 +215,11 @@ public class Main {
         return messageByte;
     }
 
+    /**
+     * Метод generateSHA1 генерирует хэшкод SHA1 (в данном случае)
+     * для файла leasing.txt
+     * @return строку, содержащую хэшкод указанного файла
+     */
     protected static String generateSHA1() {
         try {
             ProcessBuilder builder = new ProcessBuilder("openssl", "dgst", "-sha1", "leasing.txt");
@@ -181,13 +231,12 @@ public class Main {
 
             // Для чтения вывода процесса
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
 
-            // Читает каждую строку вывода процесса
-            while ((line = reader.readLine()) != null) {
+            // Читает строку вывода процесса
+            String line = reader.readLine();
+            if (line != null) {
                 return line.substring(line.indexOf("= ") + 2);
             }
-
             return null;
         } catch (IOException e) {
             throw new RuntimeException(e);
